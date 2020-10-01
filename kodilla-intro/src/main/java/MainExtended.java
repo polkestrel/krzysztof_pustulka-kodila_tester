fbc1b939910c576e94c3b8748cca81357565c0ce
Krzysztof Pustulka <polkestrel@gmail.com>
2020-10-01 11:36:36 +0200

    Prework - Init Ver for Mentor

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Krzysztof";
+        int myAge = 40;
+        double myHeight = 1.80;
+        char carClass = 'B';
+        boolean isMale = true;
+        float myCalculation = 1.f / 81;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(carClass);
+        System.out.println(isMale);
+        System.out.println(myCalculation);
+    }
+}
