800204492feb28e64a04510cb0320457108abe4d
Krzysztof Pustulka <polkestrel@gmail.com>
2020-10-05 12:27:35 +0200

    second ver

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {                            // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");          // [3]
+        System.out.println("Krzysztof");             // [4]
+        System.out.println("40");                    // [5]
+        System.out.println("P.");                    // [6]
+    }
 }
