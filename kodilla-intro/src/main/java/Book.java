598274296960aa24e353be405a954ab8ba11a25f
Krzysztof Pustulka <polkestrel@gmail.com>
2020-10-21 22:21:31 +0200

    Moduł 2.7 - zmienne i metody statyczne - zadanie

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,7 +1,12 @@
 public class Book {
-    private static Book book;
+
     private String author;
     private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
 
     public String getAuthor() {
         return author;
@@ -12,11 +17,10 @@ public class Book {
     }
 
     static Book of(String author, String title) {
-        return book;
+        return new Book(author, title);
     }
 
     public static void main(String[] args) {
-
             Book book = Book.of("Isaac Asimov", "The Galaxy");
             System.out.println(book.getAuthor() + " " + book.getTitle());
         }
