d720761b2715d5712c3a79ff3be2a1bfdab3b02a
Krzysztof Pustulka <polkestrel@gmail.com>
2020-10-28 22:51:06 +0100

    Moduł 2.6 - Zamiana kodu strukturalnego na obiektowy (refaktoryzacja) - zadanie

--- kodilla-intro/src/main/java/Osoba.java
+++ kodilla-intro/src/main/java/Osoba.java
@@ -9,18 +9,4 @@ public class Osoba {
         this.age = age;
         this.height = height;
     }
-
-    public static void main(String[] args) {
-        Osoba adam = new Osoba("Adam", 40.5, 178);
-
-        Osoba[] osoby = {adam};
-
-        for (int i = 0; i < osoby.length; i++) {
-            if (osoby[i].age > 30 && osoby[i].height > 160) {
-                System.out.println("User is older than 30 and higher than 160cm");
-            } else {
-                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-            }
-        }
-    }
 }
