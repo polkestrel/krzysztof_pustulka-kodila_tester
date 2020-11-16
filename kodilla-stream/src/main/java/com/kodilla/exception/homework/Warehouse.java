0705a5b4acdc8bbcddb5b2926b25bdeac23d54e4
Krzysztof Pustulka <polkestrel@gmail.com>
2020-11-16 22:16:25 +0100

    Moduł 6.4 - Wyjątki - zadanie (obsługa błędów)

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Warehouse {
+
+    private static List<Order> ordersList = new ArrayList<>();
+
+    public static void addOrder(Order order) {
+        ordersList.add(order);
+        System.out.println("Order number: " + order + " has been just added.");
+    }
+
+    public static Order getOrder(String number) throws OrderDoesntExistException {
+        System.out.println("Now you are looking for order number: " + number);
+        return ordersList
+                .stream()
+                .filter(order -> order.getNumber().equals(order))
+                .findFirst().orElseThrow(() -> new OrderDoesntExistException());
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -0,0 +1,29 @@
+package com.kodilla.exception.homework;
+
+public class WarehouseApp {
+    public static void main(String[] args) {
+        Warehouse.addOrder(new Order("1200"));
+        Warehouse.addOrder(new Order("10"));
+        Warehouse.addOrder(new Order("15"));
+        Warehouse.addOrder(new Order("12"));
+        Warehouse.addOrder(new Order("100"));
+
+        System.out.println("------------------------");
+
+        try {
+            Warehouse.getOrder("5");
+        } catch (OrderDoesntExistException e) {
+            System.out.println("Sorry the order number that you have just typed - doesn't exist in our database. Please choose another one.");
+        } finally {
+            System.out.println("The process has been finalized.");
+        }
+
+
+        /*
+        W metodzie main dodaj kilka zamówień do hurtowni
+        przy pomocy metody addOrder(), a następnie spróbuj wyszukać jakieś
+        nieistniejące zamówienie.
+        Przygotuj odpowiednią obsługę błędów.
+         */
+    }
+}
