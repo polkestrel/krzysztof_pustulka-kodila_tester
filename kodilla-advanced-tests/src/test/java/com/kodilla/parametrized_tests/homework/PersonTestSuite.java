0de473eb1d26ee3300ea0a382e02ca95d7c54e58
Krzysztof Pustulka <polkestrel@gmail.com>
2020-11-26 20:38:24 +0100

    Moduł 7.3 - added one extra test

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,7 +11,7 @@ class PersonTestSuite {
 
     @ParameterizedTest
     @MethodSource(value = "com.kodilla.parametrized_tests.homework.PersonSources#provideDataForDisplayingEachNotificationFromEachBmiRange")
-    public void shouldCalculateBMIForEachRange(Person person) {
+    public void shouldCalculateBMIForEachRange(Person person) throws Exception {
         System.out.println(person.getBMI());
         //System.out.println("----------------------");
         //Stream<String> setOfAllNotificationsFromTheMethodPersonSource = new Stream<>(Arrays.asList(person.getBMI()));     // Wszystkie komunikaty umieszczam w zbiorze danych
@@ -18,12 +19,23 @@ class PersonTestSuite {
         Stream<String> expectedNotifications= Stream.of("Very severely underweight","Severely underweight","Underweight","Normal (healthy weight)",
                 "Overweight","Obese Class I (Moderately obese)", "Obese Class II (Severely obese)","Obese Class III (Very severely obese)",
                 "Obese Class IV (Morbidly Obese)","Obese Class V (Super Obese)","Obese Class VI (Hyper Obese)");
-        boolean match = expectedNotifications.anyMatch(un -> un.equals(person.getBMI()));
+        boolean match = expectedNotifications.anyMatch(un -> {
+            try {
+                return un.equals(person.getBMI());
+            } catch (Exception e) {
+                return false;
+            }
+        });
         assertTrue(match);
     }
 
-    // @ParameterizedTest
-    // @MethodSource(value = "com.kodilla.parametrized_tests.homework.PersonSources#providingDataContainingZero")
-    // public void shouldNotCalculateBMIForDenominatorEqualToZero(Person person) {
-    // }
+     @ParameterizedTest
+     @MethodSource(value = "com.kodilla.parametrized_tests.homework.PersonSources#providingDataWithDenominatorEqualToZero")
+     public void shouldNotCalculateBMIForDenominatorEqualToZero(Person person) {
+        try {
+            System.out.println(person.getBMI());
+        } catch (Exception e) {
+            System.out.println("Don't try to divide by zero");
+        }
+     }
 }
