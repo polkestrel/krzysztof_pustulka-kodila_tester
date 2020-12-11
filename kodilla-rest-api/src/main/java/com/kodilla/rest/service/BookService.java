37104dd3ec6277ffc25a96536b04ebbe59bef63a
Krzysztof Pustulka <polkestrel@gmail.com>
2020-12-11 10:05:46 +0100

    Moduł 10.3 - REST API - rozbudowa API

---
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/service/BookService.java
@@ -0,0 +1,26 @@
+package com.kodilla.rest.service;
+
+import com.kodilla.rest.domain.BookDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    // @Service nad klasą informuje Springa o tym, że ta klasa jest beanem
+    // i jej obiekt ma zostać utworzone w kontenerze Springa.
+@Service
+public class BookService {
+    private List<BookDto> books = new ArrayList<>();
+
+    public List<BookDto> getBooks() {
+        return books;
+    }
+
+    public void addBook(BookDto bookDto) {
+        books.add(bookDto);
+    }
+
+    public void removeBook(BookDto bookDto) {
+        books.remove(bookDto);
+    }
+}
