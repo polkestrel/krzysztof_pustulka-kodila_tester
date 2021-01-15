53a869cba1c71601fbedd1d898605b7bdd7da138
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-15 21:10:42 +0100

    Moduł 14.4 - Cucumber - zadanie (rozszerzenie scenariuszy)

--- kodilla-cucumber/src/main/java/wallet/Wallet.java
+++ kodilla-cucumber/src/main/java/wallet/Wallet.java
@@ -2,9 +2,13 @@ package wallet;
 
 public class Wallet {
     private int balance = 0;
+    private int display;
+    private int initialBalance;
+    private String comment;
+    //private String negativeComment;
+    //private String positiveComment;
 
     public Wallet() {
-
     }
 
     public void deposit(int money) {
@@ -13,5 +17,30 @@ public class Wallet {
 
     public int getBalance() {
         return balance;
+    }
+
+    public void displayBalance (int currentMoney) {
+        this.display = currentMoney;
+    }
+
+    public int getInitialBalance () {
+        this.initialBalance = display;
+        return initialBalance;
+    }
+
+    public void debit(int money) {
+        this.balance = balance - money;
+    }
+
+    public String withdrawFromWallet(int withdrawMoney) {
+        if (withdrawMoney > getInitialBalance()) {
+            initialBalance = initialBalance;
+            comment = "You don't have enough money in your wallet !";
+        }
+        else if (withdrawMoney <= getInitialBalance()) {
+            initialBalance = initialBalance - withdrawMoney;
+            comment = "You have just withdrawed money.";
+        }
+        return comment;
     }
 }
