ee1a345b979a9832d17f7ccb9316e6eac772e205
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-21 10:18:23 +0100

    Zadanie 15.2 - Analiza kodu i testy integracyjne - zadanie (praca z JaCoCo i SonarLint

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -1,15 +1,55 @@
 package com.kodilla.jacoco;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-public class YearTest {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class YearTest {
 
     @Test
-    public void shouldBeLeapYearIfDivisibleBy400() {
+    void shouldBeLeapYearIfDivisibleBy4and100and400() {
 
         //given
         Year year = new Year(1600);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    void shouldNotBeLeapYearIfItIsNotDivisibleBy4and100and400() {
+
+        //given
+        Year year = new Year(1111);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(!isLeapYear);
+    }
+
+    @Test
+    void shouldNotBeLeapYearIfDivisibleBy4and100() {
+
+        //given
+        Year year = new Year(1500);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(!isLeapYear);
+    }
+
+    @Test
+    void shouldBeLeapYearIfDivisibleBy4only() {
+
+        //given
+        Year year = new Year(1528);
 
         //when
         boolean isLeapYear = year.isLeap();
